697dfd59c2276e93dd2ab8c027fb1a23e33907bf
Jigar Dafda <jigar.dafda@gmail.com>
2022-08-04 11:53:29 +0000

    [Auto Generated]

---
+++ github_deploy.js
@@ -0,0 +1,37 @@
+const shell = require("shelljs");
+
+// Git Setup
+(async function() {
+  console.log(`[EMERGE Builder] Repo Directory Ready`);
+  const GitSetupCommand = [
+    `git config --global user.email "jigar.dafda@gmail.com"`,
+    `git config --global user.name "Jigar Dafda"`,
+    `git config pager.diff false`,
+  ].join("\n");
+  // shell.exec(GitSetupCommand);
+
+  const { GITHUB_USERNAME, GITHUB_PERSONAL_TOKEN } = process.env;
+  console.log("GITHUB_USERNAME",GITHUB_USERNAME);
+  console.log("GITHUB_PERSONAL_TOKEN", GITHUB_PERSONAL_TOKEN);
+
+  let cloneCommand = `git clone https://${GITHUB_USERNAME}:${GITHUB_PERSONAL_TOKEN}@github.com/gofynd/Emerge.git`;
+  const command = `
+    echo "Deploying Emerge on Github"
+    ${cloneCommand}
+    cd Emerge
+    ${shell.exec(GitSetupCommand)}
+    git checkout master || git checkout -b master
+    rm -rf ./*
+    cp -R  ./../* ./
+    echo "-----------------copied files------------------"
+    ls -la
+    echo "-----------------------------------"
+    git add .
+    git commit -m "[Auto Generated]"
+    ls -la
+    git remote set-url origin https://${GITHUB_USERNAME}:${GITHUB_PERSONAL_TOKEN}@github.com/gofynd/Emerge.git
+    git push -u origin master
+`;
+  console.log(`Emerge Builder Done `);
+  shell.exec(command);
+})();
---
+++ theme/sections/extension.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="section-extension-root">
+    <!-- Extension Slot -->
+    <fdk-extension :templates="templates" />
+  </div>
+</template>
+
+<!-- #region  -->
+<settings>
+{
+    "name": "section_extension",
+    "label": "Extensions",
+    "props": [
+        {
+          "type": "extension",
+          "id": "extension",
+          "label": "Extension Positions",
+          "info": "Handle extension in these positions",
+          "positions": [
+            {
+              "value": "section_extension",
+              "text": ""
+            }
+          ],
+          "default": {}
+        }
+    ]
+}
+</settings>
+<!-- #endregion -->
+
+
+<script>
+export default {
+  props: ["settings"],
+  computed: {
+    templates() {
+      return this.settings.props?.extension?.value?.["section_extension"] || []
+    }
+  }
+};
+</script>
+
+<style></style>
